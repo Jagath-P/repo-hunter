4ca7e6953330ebced33bc122dcfbf776377cad82
Sarthak Shrivastava <sarthak776@gmail.com>
2025-11-12 16:07:09 +0530

    Added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./components/ThemeToggle";
 
 import "./App.css";
 import Topbar from "./components/Topbar";
-
+import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,7 +14,7 @@ function App() {
       <Topbar />
       <h1 className="mb-[20px] text-3xl font-bold underline">Repo Hunter</h1>
       <h2>Your place for hunting GitHub Repos</h2>
-      
+      <Footer />
     </div>
   );
 }
